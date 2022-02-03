90e63a7cc1295bace5ab87134efe07987317a7b7
devanujpatel <1909devpatel@gmail.com>
2022-02-03 11:19:05 +0530

    enter new study logs

--- study_logs.py
+++ study_logs.py
@@ -1,3 +1,5 @@
+import json
+
 import verify_input
 
 
@@ -21,7 +23,27 @@ def my_init():
 
         if user_input == 1:
             # enter new logs
-            pass
+            log_name = input("Enter log name: ")
+            log_date = input("Enter date in dd/mm/yyyy format: ")
+
+            while True:
+
+                hours = input("Number of hours studied: ")
+                try:
+                    hours = float(hours)
+                    break
+                except ValueError:
+                    print("Invalid input")
+                    continue
+
+            with open("database.json", "r") as db:
+                study_logs = json.loads(db.read())
+                print(study_logs)
+                study_logs["study_logs"].append({"name": log_name, "date": log_date, "hours": hours})
+
+            with open("database.json", "w") as db:
+                db.write(json.dumps(study_logs, indent=4))
+
         elif user_input == 2:
             # edit logs
             pass
